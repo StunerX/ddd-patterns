ca0122ec8bfa5d196eebbe4aef5e01a2eb42b809
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 18:46:45 -0300

    feat: add order item name

--- src/order-item.ts
+++ src/order-item.ts
@@ -1,5 +1,5 @@
 export class OrderItem {
-  constructor(private readonly id: string, private readonly price: number) {}
+  constructor(private readonly id: string, private name: string, private price: number) {}
 
   public getPrice(): number {
     return this.price;
--- src/order.spec.ts
+++ src/order.spec.ts
@@ -3,14 +3,14 @@ import { OrderItem } from './order-item';
 
 describe('Order unit test', () => {
   test('should throw error when id is empty', () => {
-    const items = [new OrderItem('1', 1)];
+    const items = [new OrderItem('1', 'item 1', 1)];
     expect(() => {
       new Order('', '1', items);
     }).toThrowError('Id is required');
   });
 
   test('should throw error when customer id is empty', () => {
-    const items = [new OrderItem('1', 1)];
+    const items = [new OrderItem('1', 'item 1', 1)];
     expect(() => {
       new Order('1', '', items);
     }).toThrowError('CustomerId is required');
@@ -23,8 +23,8 @@ describe('Order unit test', () => {
   });
 
   test('should calculate total', () => {
-    const item1 = new OrderItem('1', 100);
-    const item2 = new OrderItem('2', 150);
+    const item1 = new OrderItem('1', 'item 1', 100);
+    const item2 = new OrderItem('2', 'item 2', 150);
     const order = new Order('1', '1', [item1, item2]);
 
     expect(order.total()).toBe(250);
