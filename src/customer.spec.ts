9698b1480687c663553e828d3d60600b8841ab26
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 16:38:11 -0300

    test: ensure customer throws when name is empty

--- src/customer.spec.ts
+++ src/customer.spec.ts
@@ -6,4 +6,10 @@ describe('Customer unit test', () => {
       new Customer('', 'John');
     }).toThrowError('Id is required');
   });
+
+  test('should throw error when name is empty', () => {
+    expect(() => {
+      new Customer('1', '');
+    }).toThrowError('Name is required');
+  });
 });
