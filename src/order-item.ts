aecccfcffbed8d7cee370c0410577a80931a67b3
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 18:53:52 -0300

    feat: ensure getprice returns price * quantity

--- src/order-item.ts
+++ src/order-item.ts
@@ -1,7 +1,13 @@
 export class OrderItem {
-  constructor(private readonly id: string, private name: string, private price: number) {}
+  constructor(
+    private readonly id: string,
+    private name: string,
+    private price: number,
+    private productId: string,
+    private quantity: number
+  ) {}
 
   public getPrice(): number {
-    return this.price;
+    return this.price * this.quantity;
   }
 }
