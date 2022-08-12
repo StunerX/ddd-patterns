2dda5a3af5e2c416f628d4b20ab2f6f30a5ccec8
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 18:54:20 -0300

    test: ensure getprice returns price * quantity

--- src/order.spec.ts
+++ src/order.spec.ts
@@ -3,14 +3,14 @@ import { OrderItem } from './order-item';
 
 describe('Order unit test', () => {
   test('should throw error when id is empty', () => {
-    const items = [new OrderItem('1', 'item 1', 1)];
+    const items = [new OrderItem('1', 'item 1', 1, 'product_1', 2)];
     expect(() => {
       new Order('', '1', items);
     }).toThrowError('Id is required');
   });
 
   test('should throw error when customer id is empty', () => {
-    const items = [new OrderItem('1', 'item 1', 1)];
+    const items = [new OrderItem('1', 'item 1', 1, 'product_1', 2)];
     expect(() => {
       new Order('1', '', items);
     }).toThrowError('CustomerId is required');
@@ -23,10 +23,10 @@ describe('Order unit test', () => {
   });
 
   test('should calculate total', () => {
-    const item1 = new OrderItem('1', 'item 1', 100);
-    const item2 = new OrderItem('2', 'item 2', 150);
+    const item1 = new OrderItem('1', 'item 1', 100, 'product_1', 10);
+    const item2 = new OrderItem('2', 'item 2', 150, 'product_2', 3);
     const order = new Order('1', '1', [item1, item2]);
 
-    expect(order.total()).toBe(250);
+    expect(order.total()).toBe(1450);
   });
 });
