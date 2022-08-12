03f95aaa117ffbe88c3516415bbf664ac5acdf57
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 18:25:46 -0300

    test: ensure throws when validate fails

--- src/order.spec.ts
+++ src/order.spec.ts
@@ -5,7 +5,20 @@ describe('Order unit test', () => {
   test('should throw error when id is empty', () => {
     const items = [new OrderItem('1', 1)];
     expect(() => {
-      new Order('', 'John', items);
+      new Order('', '1', items);
     }).toThrowError('Id is required');
+  });
+
+  test('should throw error when customer id is empty', () => {
+    const items = [new OrderItem('1', 1)];
+    expect(() => {
+      new Order('1', '', items);
+    }).toThrowError('CustomerId is required');
+  });
+
+  test('should throw error when items is empty', () => {
+    expect(() => {
+      new Order('1', '1', []);
+    }).toThrowError('Items is required');
   });
 });
