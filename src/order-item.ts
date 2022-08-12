efe37cd3f95d58c721a781c19564877b45bb2e31
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 17:03:38 -0300

    feat: ensure throws when id is empty

--- src/order-item.ts
+++ src/order-item.ts
@@ -1,3 +1,7 @@
 export class OrderItem {
   constructor(private readonly id: string, private readonly price: number) {}
+
+  public getPrice(): number {
+    return this.price;
+  }
 }
--- src/order.ts
+++ src/order.ts
@@ -9,5 +9,15 @@ export class Order {
     this._id = id;
     this._customerId = customerId;
     this._items = items;
+
+    this.validate();
+  }
+
+  private validate() {
+    if (!this._id) throw new Error('Id is required');
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.getPrice(), 0);
   }
 }
