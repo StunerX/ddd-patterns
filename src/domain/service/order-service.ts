3d179a323f1977f8b8602d8a2f4e08091e4cc4a6
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 19:58:09 -0300

    refactor: move entities and services to domain folder

--- src/domain/service/order-service.ts
+++ src/domain/service/order-service.ts
@@ -1,7 +1,7 @@
-import { Customer } from '@src/entity/customer';
-import { Order } from '@src/entity/order';
-import { OrderItem } from '@src/entity/order-item';
 import { v4 as uuid } from 'uuid';
+import { Customer } from '../entity/customer';
+import { Order } from '../entity/order';
+import { OrderItem } from '../entity/order-item';
 
 export class OrderService {
   static placeOrder(customer: Customer, items: OrderItem[]): Order {
