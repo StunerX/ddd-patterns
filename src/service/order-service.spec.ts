6739e6a3a529b41fcd7228b7b5ee90df8bcfff33
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 19:48:12 -0300

    test: ensure place order

--- src/service/order-service.spec.ts
+++ src/service/order-service.spec.ts
@@ -1,8 +1,19 @@
+import { Customer } from '@src/entity/customer';
 import { Order } from '@src/entity/order';
 import { OrderItem } from '@src/entity/order-item';
 import { OrderService } from './order-service';
 
 describe('Order service unit tests', () => {
+  test('should place na order', () => {
+    const customer = new Customer('customer_1', 'Customer 1');
+    const item1 = new OrderItem('item_1', 'item 1', 10, 'product_1', 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
   test('should calculate total of all orders', () => {
     const item1 = new OrderItem('item_1', 'item 1', 100, 'product_1', 1);
     const item2 = new OrderItem('item_2', 'item 2', 100, 'product_2', 3);
