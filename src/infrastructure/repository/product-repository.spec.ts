e18cb12809307b68db4e24b6a32be26c15d4c7d0
stunerx <cleyton.a.santos@gmail.com>
2022-08-12 21:05:53 -0300

    test: ensure create, update, find

--- src/infrastructure/repository/product-repository.spec.ts
+++ src/infrastructure/repository/product-repository.spec.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { Product } from '@src/domain/entity/product';
 import { prismaMock } from '../db/prisma/singleton';
 import { ProductRepository } from './product-repository';
@@ -11,7 +12,7 @@ describe('Product Repository Test', () => {
     prismaMock.product.create.mockResolvedValue({
       id: '#1',
       name: 'product 1',
-      price: '100'
+      price: new Prisma.Decimal(100)
     });
 
     const output = await productRepository.create(product);
@@ -19,5 +20,33 @@ describe('Product Repository Test', () => {
     expect(output).toEqual({
       createdId: '#1'
     });
+  });
+
+  test('should update a product', async () => {
+    const productRepository = new ProductRepository();
+
+    const product = new Product('#1', 'product 1', 100);
+
+    product.changeName('product one');
+    product.changePrice(125);
+
+    prismaMock.product.update.mockResolvedValue({
+      id: '#1',
+      name: 'product one',
+      price: new Prisma.Decimal(125)
+    });
+
+    const { id } = await productRepository.update(product);
+
+    expect(id).toEqual('#1');
+
+    prismaMock.product.findFirst.mockResolvedValue({
+      id: '#1',
+      name: 'product one',
+      price: new Prisma.Decimal(125)
+    });
+
+    const editedProduct = await productRepository.find(id);
+    expect(editedProduct).toStrictEqual(product);
   });
 });
