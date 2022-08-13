1eb39b15e87a1feba52a2e1cd2cfba521def3e19
stunerx <cleyton.a.santos@gmail.com>
2022-08-13 19:28:56 -0300

    feat: ensure create product use case create a product

---
+++ src/usecase/product/create/create-product.ts
@@ -0,0 +1,16 @@
+import { ProductFactory } from '@src/domain/product/factory/product-factory';
+import { ProductRepositoryInterface } from '@src/domain/product/repository/product-repository-interface';
+import { InputCreateProductDto, OutputCreateProductDto } from './create-product-dto';
+
+export class CreateProductUseCase {
+  constructor(private readonly productRepository: ProductRepositoryInterface) {}
+  async execute(input: InputCreateProductDto): Promise<OutputCreateProductDto> {
+    const product = ProductFactory.new(input.name, input.price);
+
+    const result = await this.productRepository.create(product);
+
+    return {
+      id: result.createdId
+    };
+  }
+}
