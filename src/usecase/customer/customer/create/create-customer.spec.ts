747d950cad12b8de530c6461944095796d403b93
stunerx <cleyton.a.santos@gmail.com>
2022-08-13 18:02:07 -0300

    test: ensure create customer use case create a customer

---
+++ src/usecase/customer/customer/create/create-customer.spec.ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Customer } from '@src/domain/customer/entity/customer';
+import { CustomerRepositoryInterface } from '@src/domain/customer/repository/customer-repository-interface';
+import { CustomerRepository } from '@src/infrastructure/customer/repository/prisma/customer-repository';
+import { CreateCustomerUseCase } from './create-customer';
+
+const makeSut = () => {
+  class CustomerRepositoryStub implements CustomerRepositoryInterface {
+    create(entity: Customer): Promise<{ createdId: string }> {
+      throw new Error('Method not implemented.');
+    }
+    find(id: string): Promise<Customer | null> {
+      throw new Error('Method not implemented.');
+    }
+    findAll(): Promise<Customer[] | null> {
+      throw new Error('Method not implemented.');
+    }
+    update(entity: Customer): Promise<{ id: string }> {
+      throw new Error('Method not implemented.');
+    }
+  }
+
+  const customerRepositoryStub = new CustomerRepositoryStub();
+  const createCustomerUseCase = new CreateCustomerUseCase(customerRepositoryStub);
+
+  return {
+    customerRepositoryStub,
+    createCustomerUseCase
+  };
+};
+
+describe('Test find customer use case', () => {
+  test('should find a customer', async () => {
+    const { customerRepositoryStub, createCustomerUseCase } = makeSut();
+
+    jest.spyOn(customerRepositoryStub, 'create').mockImplementationOnce(async () => {
+      return { createdId: 'any_id' };
+    });
+
+    const input = {
+      name: 'customer 1',
+      address: {
+        street: 'street 1',
+        number: 222,
+        zipCode: '13222-221',
+        city: 'Curitiba'
+      }
+    };
+
+    const output = {
+      id: 'any_id'
+    };
+
+    const result = await createCustomerUseCase.execute(input);
+
+    expect(result).toStrictEqual(output);
+  });
+
+  // test('should throws if customer repository throws', () => {
+  //   const { customerRepositoryStub, findCustomerUseCase } = makeSut();
+
+  //   jest.spyOn(customerRepositoryStub, 'find').mockImplementationOnce(() => {
+  //     throw new Error('Customer not found');
+  //   });
+
+  //   const input = {
+  //     id: '#1'
+  //   };
+
+  //   const result = findCustomerUseCase.execute(input);
+
+  //   expect(result).rejects.toThrowError();
+  // });
+});
