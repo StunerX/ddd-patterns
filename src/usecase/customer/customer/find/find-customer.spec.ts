47eea3d4196c7d9f62a80cd7e37c5bbf04ae651b
stunerx <cleyton.a.santos@gmail.com>
2022-08-13 17:36:54 -0300

    chore: use stubs to test customer use case

--- src/usecase/customer/customer/find/find-customer.spec.ts
+++ src/usecase/customer/customer/find/find-customer.spec.ts
@@ -1,13 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Customer } from '@src/domain/customer/entity/customer';
+import { CustomerRepositoryInterface } from '@src/domain/customer/repository/customer-repository-interface';
 import { CustomerRepository } from '@src/infrastructure/customer/repository/prisma/customer-repository';
 import { FindCustomerUseCase } from './find-customer';
 
+const makeSut = () => {
+  class CustomerRepositoryStub implements CustomerRepositoryInterface {
+    create(entity: Customer): Promise<{ createdId: string }> {
+      throw new Error('Method not implemented.');
+    }
+    find(id: string): Promise<Customer | null> {
+      throw new Error('Method not implemented.');
+    }
+    findAll(): Promise<Customer[] | null> {
+      throw new Error('Method not implemented.');
+    }
+    update(entity: Customer): Promise<{ id: string }> {
+      throw new Error('Method not implemented.');
+    }
+  }
+
+  const customerRepositoryStub = new CustomerRepositoryStub();
+  const findCustomerUseCase = new FindCustomerUseCase(customerRepositoryStub);
+
+  return {
+    customerRepositoryStub,
+    findCustomerUseCase
+  };
+};
+
 describe('Test find customer use case', () => {
   test('should find a customer', async () => {
-    const customerRepository = new CustomerRepository();
-    const findCustomerUseCase = new FindCustomerUseCase(customerRepository);
+    const { customerRepositoryStub, findCustomerUseCase } = makeSut();
 
-    jest.spyOn(customerRepository, 'find').mockImplementationOnce(async () => {
+    jest.spyOn(customerRepositoryStub, 'find').mockImplementationOnce(async () => {
       const customer = new Customer('#1', 'customer 1');
       customer.changeAddress('street 1', 222, '13222-221', 'Curitiba');
       return customer;
