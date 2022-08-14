f850a09988173fc46245bea6d4dc331f7aa2c5d3
stunerx <cleyton.a.santos@gmail.com>
2022-08-13 22:09:22 -0300

    test: create and list E2E /customer

---
+++ src/infrastructure/api/__tests__/product.test.ts
@@ -0,0 +1,72 @@
+import { Prisma } from '@prisma/client';
+import { prismaMock } from '@src/infrastructure/db/prisma/singleton';
+import request from 'supertest';
+import { app } from '../express';
+
+describe('E2E test for product', () => {
+  test('should create a product', async () => {
+    prismaMock.product.create.mockResolvedValue({
+      id: 'any_id',
+      name: 'any_name',
+      price: new Prisma.Decimal(1)
+    });
+
+    const response = await request(app).post('/product').send({
+      name: 'any_name',
+      price: 1
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({
+      id: 'any_id'
+    });
+  });
+
+  test('should return 500 if product is invalid', async () => {
+    prismaMock.product.create.mockResolvedValue({
+      id: 'any_id',
+      name: 'any_name',
+      price: new Prisma.Decimal(1)
+    });
+
+    const response = await request(app).post('/product').send({
+      name: '',
+      price: 1
+    });
+
+    expect(response.status).toBe(500);
+  });
+
+  test('should return all products', async () => {
+    prismaMock.product.findMany.mockResolvedValue([
+      {
+        id: '#1',
+        name: 'any_name#1',
+        price: new Prisma.Decimal(1)
+      },
+      {
+        id: '#2',
+        name: 'any_name#2',
+        price: new Prisma.Decimal(2)
+      }
+    ]);
+
+    const response = await request(app).get('/product').send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({
+      products: [
+        {
+          id: '#1',
+          name: 'any_name#1',
+          price: 1
+        },
+        {
+          id: '#2',
+          name: 'any_name#2',
+          price: 2
+        }
+      ]
+    });
+  });
+});
