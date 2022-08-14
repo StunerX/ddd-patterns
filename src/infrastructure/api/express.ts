97db4063a3ed91b3e47b3d03d3829042dd10a4e7
stunerx <cleyton.a.santos@gmail.com>
2022-08-13 22:08:55 -0300

    feat: create and list E2E /customer

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express';
 import { customerRoute } from './routes/customer.route';
+import { productRoute } from './routes/product.route';
 
 export const app: Express = express();
 
 app.use(express.json());
 
 app.use('/customer', customerRoute);
+app.use('/product', productRoute);
