45ce73fac253966e3e374a64518236f2747a9864
stunerx <cleyton.a.santos@gmail.com>
2022-08-13 21:57:12 -0300

    feat: ensure E2E /customer list all customers

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,5 +1,6 @@
 import { CustomerRepository } from '@src/infrastructure/customer/repository/prisma/customer-repository';
 import { CreateCustomerUseCase } from '@src/usecase/customer/create/create-customer';
+import { ListCustomerUseCase } from '@src/usecase/customer/list/list-customer';
 import express, { Request, Response } from 'express';
 export const customerRoute = express.Router();
 
@@ -12,6 +13,17 @@ customerRoute.post('/', async (request: Request, response: Response) => {
       address: { street, number, zipCode, city }
     };
     const output = await usecase.execute(customerDto);
+    response.status(200).json(output);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+customerRoute.get('/', async (request: Request, response: Response) => {
+  try {
+    const usecase = new ListCustomerUseCase(new CustomerRepository());
+
+    const output = await usecase.execute();
     response.status(200).json(output);
   } catch (error) {
     response.status(500).send(error);
